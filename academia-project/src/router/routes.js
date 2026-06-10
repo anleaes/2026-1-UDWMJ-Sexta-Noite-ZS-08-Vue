b8db4ec6a63b674ccf9321010137ebe83487c83d
Nathan Ferreira <nathansferreira469@gmail.com>
2026-06-10 00:09:47 -0300

    ajuste rota

--- academia-project/src/router/routes.js
+++ academia-project/src/router/routes.js
@@ -5,7 +5,7 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: 'alunos', component: () => import('pages/AlunosPage.vue') },
-      { path: 'instrutores', component: () => import('pages/InstrutoresPage.vue') }
+      { path: 'instrutores', component: () => import('pages/InstrutoresPage.vue') },
       { path: 'exercicios', component: () => import('pages/ExerciciosPage.vue') },
       { path: 'treinos', component: () => import('pages/TreinosPage.vue') },
     ],
